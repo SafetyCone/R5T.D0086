f60dba467e76493cd228eda668fcb2aad407bde1
David Coats <david@getsafetycone.com>
2021-11-03 16:02:28 -0700

    Add IConfigurationConfigurer service definition. Remove unneeded service related libraries.

--- source/R5T.D0086.Base/Code/Services/Definitions/IConfigurationConfigurer.cs
+++ source/R5T.D0086.Base/Code/Services/Definitions/IConfigurationConfigurer.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Threading.Tasks;
 
-using R5T.Dacia;
+using Microsoft.Extensions.Configuration;
+
+using R5T.T0064;
 
 
 namespace R5T.D0086
 {
     [ServiceDefinitionMarker]
-    public interface IConfigurationConfigurer
+    public interface IConfigurationConfigurer : IServiceDefinition
     {
+        Task ConfigureConfiguration(IConfigurationBuilder configurationBuilder);
     }
 }
